9a51649f1af3b2d1e8bca4156e0a5e368089a68a
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-09-23 13:23:10 +0530

    Css Updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import "./App.css";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Bharat Sharma</h1>
-      </header>
+      <h1>Bharat Sharma</h1>
     </div>
   );
 }

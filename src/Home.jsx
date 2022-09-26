73d65fd6c1615212cab43c2262150a0c5065e692
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-09-26 16:47:13 +0530

    Updated

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { React, useState } from 'react'
 import { Grid, TextField, Button } from '@mui/material';
+import CurrencyRupeeTwoToneIcon from '@mui/icons-material/CurrencyRupeeTwoTone';
 
 const Home = () => {
+
+    const [principal, setprincipalAmount] = useState();
+    const [rate, setRate] = useState();
+    const [time, setTime] = useState();
+    const [simpleInterest, setsimpleInterest] = useState();
+    const [amount, setAmount] = useState();
+
+    function handleRateChange(event) {
+        setRate(event.target.value)
+    }
+
+    function handleSubmit() {
+        var si = (principal * rate * time) / 100
+        var amt = parseFloat(si) + parseFloat(principal)
+
+        setsimpleInterest(si)
+        setAmount(amt)
+    }
+
     return (
         <div style={{ display: "flex", justifyContent: "center", alignItems: "center", padding: "20px" }}>
             <div style={{ width: "36%", border: "2px solid black", padding: 12, borderRadius: "32px" }}>
@@ -10,16 +30,20 @@ const Home = () => {
                         <h2 style={{ display: "flex", justifyContent: "center", alignItems: "center", textDecoration: "underline" }}>Simple Interest Calculator</h2>
                     </Grid>
                     <Grid xs={12} item>
-                        <TextField fullWidth label="Enter Principal Amount" />
+                        <TextField onChange={(event) => setprincipalAmount(event.target.value)} fullWidth label="Enter Principal Amount" />
                     </Grid>
                     <Grid xs={12} item>
-                        <TextField fullWidth label="Enter Time(in years)" />
+                        <TextField onChange={(event) => setTime(event.target.value)} fullWidth label="Enter Time(in years)" />
                     </Grid>
                     <Grid xs={12} item>
-                        <TextField fullWidth label="Enter Rate of Interest" />
+                        <TextField onChange={handleRateChange} fullWidth label="Enter Rate of Interest" />
                     </Grid>
                     <Grid xs={12} item style={{ paddingTop: "24px" }}>
                         <Button variant='outlined' color='primary' fullWidth onClick={handleSubmit}>Calculate</Button>
+                    </Grid>
+                    <Grid xs={12} item style={{ paddingTop: "24px" }}>
+                        <h2>Simple Interest is {simpleInterest}</h2>
+                        <h2>Amount after interest is {amount}</h2>
                     </Grid>
                 </Grid>
             </div>
